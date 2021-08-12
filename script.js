24a267ebd24882fcee78ff01bbed9f9b95366f20
Mohansai668 <86096871+Mohansai668@users.noreply.github.com>
2021-08-12 13:23:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("onblub")) {
+      image.src = "off blub.png";
+    } else {
+      image.src = "onblub.png";
+    }
+  }
+  function changeImage1() {
+    var image = document.getElementById('myImage1');
+    if (image.src.match("bulbgreen")) {
+      image.src = "off blub.png";
+    } else {
+      image.src = "pic_bulbgreen_png.png";
+    }
+    }
+    function changeImage2() {
+    var image = document.getElementById('myImage2');
+    if (image.src.match("bulbred")) {
+      image.src = "off blub.png";
+    } else {
+      image.src = "pic_bulbred_png.png";
+    }
+  }
+  function changeImage3() {
+    var image = document.getElementById('myImage3');
+    if (image.src.match("mohan1")) {
+      image.src = "off blub.png";
+    } else {
+      image.src = "pic_mohan1.gif";
+    }
+  }
